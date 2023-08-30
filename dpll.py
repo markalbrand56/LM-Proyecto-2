a85e0bf3d5c80d247aa69493567f6a3ce9dd2741
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-08-30 12:30:10 -0600

    Update dpll.py
    cambio de literales a las del pdf

--- dpll.py
+++ dpll.py
@@ -4,26 +4,26 @@ def __select_literal(cnf):
             return literal[0]
 
 
-def dpll(cnf, assignments={}):
+def dpll(b, assignments={}):
     ## Si B es vacía, entonces regresar True e I
-    if len(cnf) == 0:
+    if len(b) == 0:
         return True, assignments
 
     ## Si alguna cláusula/disyunción es vacía, entonces regresar False
-    if any([len(c) == 0 for c in cnf]):
+    if any([len(c) == 0 for c in b]):
         return False, None
 
     ## Poner en forma positiva
-    l = __select_literal(cnf)
+    l = __select_literal(b)
 
 
     ## Eliminar todas las clausulas que contengan l en B.
     ## Eliminar las ocurrencias en la clausula complementaria de L en B.
     ## Así, construir B'.
-    new_cnf = [c for c in cnf if (l, True) not in c]
-    new_cnf = [c.difference({(l, False)}) for c in new_cnf]
+    new_b = [c for c in b if (l, True) not in c]
+    new_b = [c.difference({(l, False)}) for c in new_b]
     ## I' = I U DPLL(B', I')
-    sat, vals = dpll(new_cnf, {**assignments, **{l: True}})
+    sat, vals = dpll(new_b, {**assignments, **{l: True}})
 
     ## Si el resultado de I es verdadero, entonces regresar True e I.
     if sat:
@@ -32,10 +32,10 @@ def dpll(cnf, assignments={}):
     ## Eliminar todas las clausulas que contengan -l en B.
     ## Eliminar las ocurrencias en la clausula complementaria de -L en B.
     ## Así, construir B''.
-    new_cnf = [c for c in cnf if (l, False) not in c]
-    new_cnf = [c.difference({(l, True)}) for c in new_cnf]
+    new_b = [c for c in b if (l, False) not in c]
+    new_b = [c.difference({(l, True)}) for c in new_b]
     ## I'' = I U DPLL(B'', I'')
-    sat, vals = dpll(new_cnf, {**assignments, **{l: False}})
+    sat, vals = dpll(new_b, {**assignments, **{l: False}})
     ## Si el resultado de I es verdadero, entonces regresar True e I.
     if sat:
         return sat, vals
@@ -51,27 +51,27 @@ if __name__ == "__main__":
     # {{-r}, {-q, -r}, {-p, q, -r}, {q}} => [['-r'], ['-q', '-r'], ['-p', 'q', '-r'], ['q']]
 
     input_str = input("\nIngrese las cláusulas en el formato {{p}, {-q, -r}, ...}: ")
-    cnf = []
+    b = []
 
     for clause in input_str.split('}, {'):
         clause = clause.replace('{', '').replace('}', '')  # Eliminar llaves
         clause = clause.split(',')  # Separar literales
         clause = [literal.strip() for literal in clause]  # Eliminar espacios en blanco
-        cnf.append(clause)  # Agregar cláusula a la lista
+        b.append(clause)  # Agregar cláusula a la lista
 
-    cnf = [{(literal[-1], not literal.startswith('-')) for literal in clause} for clause in cnf]
-    print(f"\tClausulas: {cnf}")
+    b = [{(literal[-1], not literal.startswith('-')) for literal in clause} for clause in b]
+    print(f"\tClausulas: {b}")
 
     # Identificar todas las variables presentes en las cláusulas
     variables = set()
-    for clause in cnf:
+    for clause in b:
         for literal in clause:
             variable = literal[0].lstrip('-')
             variables.add(variable)
 
     print(f"\tVariables: {variables}")
 
-    is_satisfiable, assignment = dpll(cnf)
+    is_satisfiable, assignment = dpll(b)
 
     if is_satisfiable:
         print(f"\tSatisfacible: {is_satisfiable}")
